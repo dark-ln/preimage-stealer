00078117c97b94c794ff0a0047c7049ec1190d9f
felix-urban <felix.urban@tutanota.com>
2022-11-12 19:25:34 -0600

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+// This program accepts four arguments: host, port, cert file, macaroon file
+
+use sha2::Digest;
+use sha2::Sha256;
+
+#[tokio::main]
+async fn main() {
+    let mut args = std::env::args_os();
+    args.next().expect("not even zeroth arg given");
+    let host = args
+        .next()
+        .expect("missing arguments: host, port, cert file, macaroon file");
+    let port = args
+        .next()
+        .expect("missing arguments: port, cert file, macaroon file");
+    let cert_file = args
+        .next()
+        .expect("missing arguments: cert file, macaroon file");
+    let macaroon_file = args.next().expect("missing argument: macaroon file");
+    let host: String = host.into_string().expect("host is not UTF-8");
+    let port: u32 = port
+        .into_string()
+        .expect("port is not UTF-8")
+        .parse()
+        .expect("port is not u32");
+    let cert_file: String = cert_file.into_string().expect("cert_file is not UTF-8");
+    let macaroon_file: String = macaroon_file
+        .into_string()
+        .expect("macaroon_file is not UTF-8");
+
+    // Connecting to LND requires only host, port, cert file, macaroon file
+    let mut client = tonic_openssl_lnd::connect(host, port, cert_file, macaroon_file)
+        .await
+        .expect("failed to connect");
+
+    let info = client
+        .lightning()
+        // All calls require at least empty parameter
+        .get_info(tonic_openssl_lnd::lnrpc::GetInfoRequest {})
+        .await
+        .expect("failed to get info");
+
+    // We only print it here, note that in real-life code you may want to call `.into_inner()` on
+    // the response to get the message.
+    println!("{:#?}", info);
+
+    // HTLC event stream part
+    let mut htlc_event_stream = client
+        .router()
+        .subscribe_htlc_events(tonic_openssl_lnd::routerrpc::SubscribeHtlcEventsRequest {})
+        .await
+        .expect("Failed to call subscribe_htlc_events")
+        .into_inner();
+
+    println!("starting htlc event subscription");
+
+    while let Some(htlc_event) = htlc_event_stream
+        .message()
+        .await
+        .expect("Failed to receive invoices")
+    {
+        println!("{:#?}", htlc_event);
+        if let Some(event) = htlc_event.event {
+            match event {
+                tonic_openssl_lnd::routerrpc::htlc_event::Event::SettleEvent(settle_event) => {
+                    let mut hasher = Sha256::new();
+                    hasher.update(&settle_event.preimage);
+                    let payment_hash = hasher.finalize();
+                    println!(
+                        "got preimage {} from payment hash {}",
+                        hex::encode(settle_event.preimage),
+                        hex::encode(payment_hash)
+                    )
+                }
+                _ => {}
+            };
+        }
+    }
+
+    // HTLC interceptor part
+    /*
+    let (tx, rx) = tokio::sync::mpsc::channel::<
+        tonic_openssl_lnd::routerrpc::ForwardHtlcInterceptResponse,
+    >(1024);
+    let stream = tokio_stream::wrappers::ReceiverStream::new(rx);
+
+    let mut htlc_stream = client
+        .router()
+        .htlc_interceptor(stream)
+        .await
+        .expect("Failed to call htlc_interceptor")
+        .into_inner();
+
+    println!("starting htlc interception");
+
+    while let Some(htlc) = htlc_stream
+        .message()
+        .await
+        .expect("Failed to receive htlc's")
+    {
+        println!("htlc {:?}", htlc);
+        let response = tonic_openssl_lnd::routerrpc::ForwardHtlcInterceptResponse {
+            incoming_circuit_key: htlc.incoming_circuit_key,
+            action: 2,        // this will resume
+            preimage: vec![], // this would be for a real preimage
+            failure_code: 0,
+            failure_message: vec![],
+        };
+        tx.send(response).await.unwrap();
+    }
+    */
+}
